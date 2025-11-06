c32f0e814d37e6e76d048ae23f2c99782f52a558
migi0401 <migi111@naver.com>
2025-11-06 20:38:24 +0900

    feat: Splash, 문의 게시판 추가

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 
-// 1. 네비게이터 컴포넌트 하나만 import 합니다.
 import AppNavigator from './src/Navigation';
+import * as SplashScreen from "expo-splash-screen";
 
-// (createNativeStackNavigator 및 모든 스크린 import는 삭제)
+SplashScreen.preventAutoHideAsync();
+
+function sleep(ms){
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function delay_splash(){
+  await SplashScreen.preventAutoHideAsync();
+  await sleep(3000);
+  await SplashScreen.hideAsync();
+}
 
 export default function App() {
+  delay_splash();
   return (
     <SafeAreaProvider>
-        {/* 2. 분리한 네비게이터 컴포넌트를 렌더링합니다. */}
         <AppNavigator />
     </SafeAreaProvider>
   );
